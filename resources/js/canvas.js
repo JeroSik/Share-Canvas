bb0e482a5ec518216bb3963e448b3532a4e35cf1
JeroSik <yoon161@purdue.edu>
2019-04-20 16:59:46 -0400

    Get mouse click

--- resources/js/canvas.js
+++ resources/js/canvas.js
@@ -3,35 +3,16 @@ var context;
 
 function initCanvas() {
     context = document.getElementById("mainCanvas").getContext("2d");
-
-    $('#canvas').mousedown(function(e){
+    $("#mainCanvas").mousedown(function(e){
         var mouseX = e.pageX - this.offsetLeft;
         var mouseY = e.pageY - this.offsetTop;
               
-        paint = true;
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-        redraw();
-      });
+        drawPixel(mouseX, mouseY);
+    });
 }
 
-function redraw(){
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-    
-    context.strokeStyle = "#df4b26";
-    context.lineJoin = "round";
-    context.lineWidth = 5;
-              
-    for(var i=0; i < clickX.length; i++) {		
-      context.beginPath();
-      if(clickDrag[i] && i){
-        context.moveTo(clickX[i-1], clickY[i-1]);
-       }else{
-         context.moveTo(clickX[i]-1, clickY[i]);
-       }
-       context.lineTo(clickX[i], clickY[i]);
-       context.closePath();
-       context.stroke();
-    }
-  }
+function drawPixel(x, y) {
+
+}
 
 initCanvas();
