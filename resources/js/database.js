54ac8c8cca4aba7b9f43a9c870dbb30bb4df73f0
JeroSik <yoon161@purdue.edu>
2019-04-22 13:51:40 -0400

    Initialize firebase

---
+++ resources/js/database.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCj6TSXMLn3j3eCORCccs5f_tBiLLai_h8",
+    authDomain: "share-canvas.firebaseapp.com",
+    databaseURL: "https://share-canvas.firebaseio.com",
+    projectId: "share-canvas",
+    storageBucket: "share-canvas.appspot.com",
+    messagingSenderId: "356431070588"
+};
+
+firebase.initializeApp(config);
