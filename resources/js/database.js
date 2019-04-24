93d3ae3f68c454dd13ba42118ce62b4985e2b296
JeroSik <yoon161@purdue.edu>
2019-04-24 17:13:59 -0400

    Add update feature

--- resources/js/database.js
+++ resources/js/database.js
@@ -28,17 +28,16 @@ function addPixel(pixel) {
 }
 
 function updatePixel(pixel) {
-    var washingtonRef = db.collection("cities").doc("DC");
+    var pixelRef = db.collection("pixels").doc("pixel.id");
 
     // Set the "capital" field of the city 'DC'
-    return washingtonRef.update({
-        capital: true
+    return pixelRef.update({
+        color: pixel.color
     })
     .then(function() {
         console.log("Document successfully updated!");
     })
     .catch(function(error) {
-        // The document probably doesn't exist.
         console.error("Error updating document: ", error);
     });
 }
