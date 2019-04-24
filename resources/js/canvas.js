427e985cb3d408d98d29d1bd3506030efa63c215
vivian-guo9 <vguo@purdue.edu>
2019-04-23 21:22:06 -0400

    add other option to mouse

--- resources/js/canvas.js
+++ resources/js/canvas.js
@@ -1,19 +1,44 @@
 var canvas;
 var context;
 var pixelColor;
+var curTool;
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var on;
 
 function initCanvas() {
     canvas = document.getElementById("mainCanvas")
-    canvas.width = 750;
-    canvas.height = 750;
+    canvas.width = 600;
+    canvas.height = 600;
 
     context = canvas.getContext("2d");
+    curTool = 'pixel'
+
     $("#mainCanvas").mousedown(function(e){
+        on = true;
         var mouseX = e.pageX - this.offsetLeft;
         var mouseY = e.pageY - this.offsetTop;
 
-        drawPixel(mouseX, mouseY);
+        if(curTool === 'pixel'){
+            drawPixel(mouseX, mouseY);
+        }else if(curTool === 'paint'){
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
+            redraw();
+        }
     });
+    $('#mainCanvas').mousemove(function(e){
+        if(on){
+          addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+          redraw();
+        }
+    });
+}
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
 }
 
 function drawPixel(x, y) {
